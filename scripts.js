14fba9d12b4a6f422ac4122d2187b4481ed8693b
LifebarKevin <kevin.portell@lifebarstudios.com>
2016-08-10 16:54:24 -0400

    Added a ton
    Corrected the “sticky nav issue”
    Did some media Queues - Some more work will need to be done.
    Added a fade affect on the greeting as the page is scrolled.
    Added link images - need to add the URL’s
    Did some spacing work and need to update it some more.

--- scripts.js
+++ scripts.js
@@ -6,5 +6,22 @@ $('document').ready(function(){
 		} else {
 			$("nav").removeClass("main-nav-scrolled");
 		}
+		if ( $(this).scrollTop() >= 160){
+			$('.lname').fadeOut('fast');
+		}else{
+			$('.lname').fadeIn('slow');
+		}
+		if ( $(this).scrollTop() >= 330){
+			$('.fname').fadeOut('fast');
+		}else{
+			$('.fname').fadeIn('slow');
+		}
+		if ( $(this).scrollTop() >= 430){
+			$('.hi').fadeOut('fast');
+			$('.im').fadeOut('fast');
+		}else{
+			$('.hi').fadeIn('slow');
+			$('.im').fadeIn('slow');
+		}
 	});
 });
